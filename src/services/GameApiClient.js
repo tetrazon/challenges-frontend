af4d2e0f7792fedf43daa6e4134202b8307c4fb2
ivan.smuniov <smunew.rfkt@gmail.com>
2022-06-27 15:34:41 +0400

    add challenges api client

---
+++ src/services/GameApiClient.js
@@ -0,0 +1,9 @@
+class GameApiClient {
+    static SERVER_URL = 'http://localhost:8091';
+    static GET_LEADERBOARD = '/leaders';
+    static leaderBoard(): Promise<Response> {
+        return fetch(GameApiClient.SERVER_URL +
+            GameApiClient.GET_LEADERBOARD);
+    }
+}
+export default GameApiClient;
